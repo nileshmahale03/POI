a34bc0095644d6f17d172633461b725a63f30b04
nileshmahale <nileshmahale03@gmail.com>
2015-11-21 21:36:55 -0800

    Xcode Project Created

---
+++ POI/POI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POI
+//
+//  Created by Nilesh Mahale on 11/21/15.
+//  Copyright © 2015 Code-Programming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
