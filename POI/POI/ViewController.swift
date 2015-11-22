730c1724f7b3e12b8dcfa1817033570d66b90a21
nileshmahale <nileshmahale03@gmail.com>
2015-11-21 23:03:51 -0800

    Map added, zoom and type button added

--- POI/POI/ViewController.swift
+++ POI/POI/ViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
+        mapView.delegate = self
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.showsUserLocation = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func zoomIn(sender: AnyObject) {
+        let userLocation = mapView.userLocation
+        
+        let region = MKCoordinateRegionMakeWithDistance(userLocation.location!.coordinate, 2000, 2000)
+        
+        mapView.setRegion(region, animated: true)
+    }
+    
+    @IBAction func changeMapType(sender: AnyObject) {
+        if mapView.mapType == MKMapType.Standard {
+            mapView.mapType = MKMapType.Satellite
+        } else {
+            mapView.mapType = MKMapType.Standard
+        }
+    }
 
+    func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
+        mapView.centerCoordinate = userLocation.location!.coordinate
+    }
 
 }
+
+
+
+
+
+
+
+
+
 
